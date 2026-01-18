a0d313ea59cb4ba691a09dd89d418e60d9b9fa47
sm-atahar-hossain <smataharhossain.bd@gmail.com>
2026-01-18 23:54:34 +0600

    completed Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"], // Update content paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
